85ac6019de7d24a54051135063a4242f928abbfe
prohladenn <kovshov.valery@gmail.com>
2024-12-06 22:33:23 +0100

    Day 6 blank

---
+++ src/prohladenn/vko/aoc2024/Day06.java
@@ -0,0 +1,31 @@
+package prohladenn.vko.aoc2024;
+
+import java.util.Scanner;
+
+public class Day06 extends AbstractDay<AbstractDay.InputLines> {
+
+    public static void main(String[] args) {
+        new Day06().run();
+    }
+
+    @Override
+    protected String getInputFileName() {
+        return "day06.txt";
+    }
+
+    @Override
+    protected InputLines readInput(Scanner sc) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    protected Object solvePart1(InputLines input) {
+        return null;
+    }
+
+    @Override
+    protected Object solvePart2(InputLines input) {
+        return null;
+    }
+
+}
